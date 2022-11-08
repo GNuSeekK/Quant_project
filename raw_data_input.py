2a7f01ec741371baf1f08f4214cd0429b3d8085a
KeesungLee <ks.lee@alcherainc.com>
2022-11-08 14:21:24 +0900

    raw_data_input update 20221104

---
+++ raw_data_input.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import mysql_control
+
+db = mysql_control.connecting('quantdb')
+df = pd.read_excel(r'C:\Users\user\Desktop\All_git\Quant_project\데이터.xlsx')
+
+# 데이터 정리
+df.columns = list(df.loc[9][df.columns[:5]]) + list(df.loc[8][df.columns[5:]])
+df = df.loc[10:]
+df[df.columns[0]] = df[df.columns[0]].apply(lambda x: x[1:])
+
+# 주기 (1Q, 2Q, 3Q, 4Q, Annual) 인데, 1~4Q는 nan값 있으면 버리고, 4Q에 매출0 있으면 버리기
+# 카카오, sk텔레콤 처럼 매출원가가 0인 기업들도 있음
+df = df.dropna('index')
